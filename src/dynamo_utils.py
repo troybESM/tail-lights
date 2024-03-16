0181285dbb4b80eadb665fe5ee7fe58e69f0d3df
troy <8160969+troybESM@users.noreply.github.com>
2024-03-16 13:38:22 -0400

    Comiit so I can test deploy

--- src/dynamo_utils.py
+++ src/dynamo_utils.py
@@ -1,6 +1,6 @@
 import os
 import boto3
-import datetime 
+import datetime
 from aws_lambda_powertools import Logger
 from boto3.dynamodb.conditions import Key
 
@@ -9,19 +9,21 @@ logger = Logger()
 ddb = boto3.resource('dynamodb')
 table = ddb.Table(os.getenv("TABLE", "No Table"))
 
-def get_ttl():
+
+def get_ttl(date):
     # Get TTL
     now = datetime.datetime.now()
-    td = datetime.timedelta(minutes=10)
+    td = datetime.timedelta(days=10)
     TTL = now + td
     return round(TTL.timestamp())
 
-def write_name(name,request_id):
+
+def add_show(show, request_id):
     # Update Count and keep the new value
     count_response = table.update_item(
         Key={
-            'pk': 'APPSTATE',
-            'sk': 'COUNT#ACCESSED'
+            'pk': 'SHOW#META',
+            'sk': 'SHOW#ACCESSED'
         },
         UpdateExpression='ADD #s :val',
         ExpressionAttributeNames={
@@ -32,26 +34,65 @@ def write_name(name,request_id):
         },
         ReturnValues="UPDATED_NEW"
     )
-    current_count = count_response['Attributes']['number']
-    
+
     # Store new Item
-    TTL = get_ttl()
-    name_response = table.put_item(
+    description = ""
+    TTL = get_ttl(date="<Show Date>")
+    response = table.put_item(
         Item={
-            'pk': f"REQ#{request_id}",
-            'sk': f"USER#{name}",
-            'number': current_count,
+            'pk': f'SHOW#{request_id}',
+            'sk': f'SHOW#<date>',
+            'approved': False,
+            'active': True,
+            'date': '',
+            'location': {
+                'address': '',
+                'city': '',
+                'state': '',
+                'zip': ''
+            },
+            'description': description,
+            'eventTimes': {
+                'reg': '',
+                'awards': ''
+            },
+            'contact': {
+                'name': '',
+                'email': '',
+                'addr': '',
+                'site': ''
+            },
+            'socialLinks': {
+                'fb': '',
+                'x': '',
+                'insta': '',
+                'mast': ''
+            },
+            'sponsors': [
+                {
+                    'name': '',
+                    'links': ''
+                }
+            ],
+            'GSI1PK': 'SHOW',
+            'GSI1SK': f'SHOW#{request_id}',
+            'GSI1Data': {
+                'description': description[0:100],
+                'date': '',
+                'location': '',
+            },
             'TTL': TTL
         }
     )
-    logger.info(f"utils response {name_response}")
-    return name_response
+    logger.info(f"Added Item: {response}")
+    return response
+
 
 def decrease_count():
     count_response = table.update_item(
         Key={
-            'pk': 'APPSTATE',
-            'sk': 'COUNT#ACCESSED'
+            'pk': 'SHOW#META',
+            'sk': 'SHOW#ACCESSED'
         },
         UpdateExpression='ADD #s :val',
         ExpressionAttributeNames={
@@ -64,6 +105,7 @@ def decrease_count():
     )
     current_count = count_response['Attributes']['number']
     return current_count
+
 
 def add_winning_number(number):
     response = table.update_item(
@@ -82,23 +124,26 @@ def add_winning_number(number):
     )
     return response
 
+
 def query_table(pk, sk):
-    response= table.query(
-        KeyConditionExpression = Key('pk').eq(pk) & Key('sk').eq(sk)
+    response = table.query(
+        KeyConditionExpression=Key('pk').eq(pk) & Key('sk').eq(sk)
     )
     return response['Items']
+
 
 def scan_gsi(sk):
-    response= table.scan(
+    response = table.scan(
         IndexName="number-name",
         Limit=200,
-        FilterExpression = Key('sk').begins_with(sk)
+        FilterExpression=Key('sk').begins_with(sk)
     )
     return response['Items']
 
+
 def query_gsi(query):
-    response= table.query(
+    response = table.query(
         IndexName="number-name",
-        KeyConditionExpression = Key('number').eq(query)
+        KeyConditionExpression=Key('number').eq(query)
     )
     return response['Items']
