77c14069c56306b00cefcab5151158d31cae7afb
troy <8160969+troybESM@users.noreply.github.com>
2024-03-19 20:21:11 -0400

    Update channel info

--- src/rss.py
+++ src/rss.py
@@ -28,21 +28,9 @@ def get(event, context):
     body = f"""
         <rss version="2.0">
         <channel>
-        <title>Sample Feed</title>
-        <description>For documentation &lt;em&gt;only&lt;/em&gt;</description>
-        <link>http://example.org/</link>
-        <pubDate>Sat, 07 Sep 2002 00:00:01 GMT</pubDate>
-        <!-- other elements omitted from this example -->
-        <item>
-        <title>First entry title</title>
-        <link>http://example.org/entry/3</link>
-        <description>Watch out for &lt;span style="background-image:
-        url(javascript:window.location='http://example.org/')"&gt;nasty
-        tricks&lt;/span&gt;</description>
-        <pubDate>Thu, 05 Sep 2002 00:00:01 GMT</pubDate>
-        <guid>http://example.org/entry/3</guid>
-        <!-- other elements omitted from this example -->
-        </item>
+        <title>Old Cars Only</title>
+        <description>For all of your car show needs</description>
+        <link>indyautoscene.tektrek.io</link>
         {''.join(show_element_list)}
         </channel>
         </rss>
